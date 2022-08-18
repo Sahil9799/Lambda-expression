02858a19f157d1b9b48aa7600885c97da0ab2e48
ADMIN <sahilbambhania@gmail.com>
2022-08-18 15:47:40 +0530

    Add lambda exp

---
+++ LambdaExpressionProgram/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace LambdaExpressionProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Lambda Expression Program\n========================================");
+
+            List<Person> personlist = new List<Person>();
+            personlist.Add(new Person("123654", "Vishwas", "Jabalpur", 18));
+            personlist.Add(new Person("123656", "Vivek", "Mumbai", 65));
+            personlist.Add(new Person("123657", "Siddhant", "Pune", 22));
+            personlist.Add(new Person("123659", "Shobhit", "Indore", 16));
+            personlist.Add(new Person("123658", "Avinash", "Bhopal", 60));
+            personlist.Add(new Person("123678", "Sagar", "Patna", 68));
+            personlist.Add(new Person("129668", "Priyanshu", "Noida", 58));
+            personlist.Add(new Person("126662", "Ankit", "Bhoma", 14));
+            
+            
+            Console.WriteLine("Retrieving Top 2 aged person from the list less than 60 years :");
+            foreach (Person person in personlist.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
+            }
+
+            Console.WriteLine("\nChecking whether any one of them teenager or not :");
+            foreach (Person person in personlist.FindAll(e => (e.Age >= 13 && e.Age <= 18)))
+            {
+                Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
+            }
+
+            Console.WriteLine("\nGetting avg age of all the people :");
+            double avgAge = personlist.Average(e => e.Age);
+            Console.WriteLine("The avg age of all the people : " + avgAge);
+
+            Console.WriteLine("\nchecking whether the person name 'Vishwas' is present or not :");
+            if (personlist.Exists(e => e.Name == "Vishwas"))
+            {
+                Console.WriteLine("Yes the person naming Vishwas is present in the list");
+            }
+
+            Console.WriteLine("\nSkipping every person whose age is less than 60 : ");
+            foreach (Person person in personlist.FindAll(e => e.Age < 60))
+            {
+                Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
+            }
+
+
+            Console.WriteLine("\nRemove Person Called 'Avinash' From List : ");
+            personlist.RemoveAll(e => e.Name == "Avinash");
+            if (personlist.TrueForAll(e => e.Name != "Avinash"))
+            {
+                Console.WriteLine("There is no one in the list called Avinash");
+            }
+        }
+    }
+}
